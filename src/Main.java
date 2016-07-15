68276d9d8bdcd985a9ade343d8e12262911fff6a
v-shevchenko <mail.shevchenko.v@gmail.com>
2016-07-15 20:24:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        for(MyDayOfWeek days:MyDayOfWeek.values()){
+            System.out.println(days.name());
+        }
+    }
+}
---
+++ src/MyDayOfWeek.java
@@ -0,0 +1,9 @@
+public enum MyDayOfWeek {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+    }
